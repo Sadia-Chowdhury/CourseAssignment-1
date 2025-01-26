115ef1410c710f61080eefc22802f6c2f2679224
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-01-27 00:06:59 +0600

    print 1 to 10 using c++

---
+++ numbers from 1-10.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi acos(-1.0)
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//cout<<fixed<<setprecision(2);
+
+    int t=10;
+    for(int i=1; i<=10; i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
